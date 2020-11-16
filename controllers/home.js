075a0a3fa21952251457a018a8f5329a28485ba8
ki-shohag <koushikur.aiub@gmail.com>
2020-11-16 15:31:58 +0600

    added customer and product management pages

--- controllers/home.js
+++ controllers/home.js
@@ -4,5 +4,11 @@ const router 	= express.Router();
 router.get('/', (req, res)=>{
 	res.render('home/index');	
 });
+router.get('/products', (req, res)=>{
+	res.render('home/product-management');	
+});
+router.get('/customers', (req, res)=>{
+	res.render('home/customer-management');	
+});
 
 module.exports = router;
